4962366ed04e618dc3ac8119b7a85b08e4c9873e
Lucemi Facundo <cemirbv@gmail.com>
2024-03-07 20:34:09 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="esc.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula PHP</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso</h1>
+        <h2>Exercícios </h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo">
+                    <h3>Modulo 01</h3>
+                    <ul>
+                        <li><a href="#">Atividade 1</a></li>
+                        <li><a href="#">Atividade 1</a></li>
+                        <li><a href="#">Atividade 1</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Modulo 01</h3>
+                    <ul>
+                        <li><a href="#">Atividade 1</a></li>
+                        <li><a href="#">Atividade 1</a></li>
+                        <li><a href="#">Atividade 1</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Modulo 01</h3>
+                    <ul>
+                        <li><a href="#">Atividade 1</a></li>
+                        <li><a href="#">Atividade 1</a></li>
+                        <li><a href="#">Atividade 1</a></li>
+                    </ul>
+                </div>
+                
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        EEEEP & INF
+    </footer>
+    
+</body>
+</html>
